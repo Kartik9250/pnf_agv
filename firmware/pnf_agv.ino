c28140fc2a77555b8cdfe730a0b854f7ae3488df
Kartik9250 <kartikrana9250@gmail.com>
2023-06-07 11:22:05 +0530

    Changed pinouts & added aruidno setup also

--- firmware/pnf_agv.ino
+++ firmware/pnf_agv.ino
@@ -11,24 +11,26 @@ const int ledChannel3 = 2;
 const int ledChannel4 = 3;
 const int resolution = 8;
 
-const int PWM1=2;
-const int PWM2=17;
-const int PWM3=22;
-const int PWM4=25;
-
-const int MD1_AIN1=0;
-const int MD1_AIN2=4;
-const int MD1_BIN1=18;
-const int MD1_BIN2=5;
-
 const int MD1_STBY=13;
+const int MD2_STBY=15;
 
-const int MD2_AIN1=15;
-const int MD2_AIN2=23;
-const int MD2_BIN1=27;
-const int MD2_BIN2=26;
+const int FR_PWM=33;
+const int FR_AIN1=22;
+const int FR_AIN2=23;
 
-const int MD2_STBY=21;
+const int FL_PWM=27;
+const int FL_BIN1=14;
+const int FL_BIN2=12;
+
+const int BR_PWM=21;
+const int BR_AIN1=19;
+const int BR_AIN2=18;
+
+const int BL_PWM=16;
+const int BL_BIN1=4;
+const int BL_BIN2=0;
+
+
 
 
 
@@ -37,20 +39,24 @@ void motorCb(const pnf_agv::Holo_agv& msg){
 //  digitalWrite(MD1_STBY, HIGH);
 //  digitalWrite(MD2_STBY, msg.motor_driver_back.stby);
 
-  digitalWrite(MD1_AIN1, msg.motor_driver_front.motor_right.IN1);
-  digitalWrite(MD1_AIN2, msg.motor_driver_front.motor_right.IN2);
+  digitalWrite(FR_AIN1, msg.motor_driver_front.motor_right.IN1);
+  digitalWrite(FR_AIN2, msg.motor_driver_front.motor_right.IN2);
+  // digitalWrite(FR_PWM, msg.motor_driver_front.motor_right.pwm);
   ledcWrite(ledChannel1, msg.motor_driver_front.motor_right.pwm);
   
-  digitalWrite(MD1_BIN1, msg.motor_driver_front.motor_left.IN1);
-  digitalWrite(MD1_BIN2, msg.motor_driver_front.motor_left.IN2);
+  digitalWrite(FL_BIN1, msg.motor_driver_front.motor_left.IN1);
+  digitalWrite(FL_BIN2, msg.motor_driver_front.motor_left.IN2);
+  // digitalWrite(FL_PWM, msg.motor_driver_front.motor_right.pwm);
   ledcWrite(ledChannel2, msg.motor_driver_front.motor_left.pwm);
   
-  digitalWrite(MD2_AIN1, msg.motor_driver_back.motor_right.IN1);
-  digitalWrite(MD2_AIN2, msg.motor_driver_back.motor_right.IN2);
+  digitalWrite(BR_AIN1, msg.motor_driver_back.motor_right.IN1);
+  digitalWrite(BR_AIN2, msg.motor_driver_back.motor_right.IN2);
+  // digitalWrite(BR_PWM, msg.motor_driver_front.motor_right.pwm);
   ledcWrite(ledChannel3, msg.motor_driver_back.motor_right.pwm);
   
-  digitalWrite(MD2_BIN1, msg.motor_driver_back.motor_left.IN1);
-  digitalWrite(MD2_BIN2, msg.motor_driver_back.motor_left.IN2);
+  digitalWrite(BL_BIN1, msg.motor_driver_back.motor_left.IN1);
+  digitalWrite(BL_BIN2, msg.motor_driver_back.motor_left.IN2);
+  // digitalWrite(BL_PWM, msg.motor_driver_front.motor_right.pwm);
   ledcWrite(ledChannel4, msg.motor_driver_back.motor_left.pwm);
   
   
@@ -71,27 +77,33 @@ void setup() {
   ledcSetup(ledChannel2, freq, resolution);
   ledcSetup(ledChannel3, freq, resolution);
   ledcSetup(ledChannel4, freq, resolution);
-
+  ledcAttachPin(FR_PWM,      ledChannel1);
+  ledcAttachPin(FL_PWM,      ledChannel2);
+  ledcAttachPin(BR_PWM,      ledChannel3);
+  ledcAttachPin(BL_PWM,      ledChannel4);
+  
   //Serial.begin (57600);
 
   pinMode(MD1_STBY, OUTPUT);
   pinMode(MD2_STBY, OUTPUT);
-  pinMode(MD1_AIN1, OUTPUT);
-  pinMode(MD1_AIN2, OUTPUT);
-  pinMode(MD1_BIN1, OUTPUT);
-  pinMode(MD1_BIN2, OUTPUT);
-  pinMode(MD2_AIN1, OUTPUT);
-  pinMode(MD2_AIN2, OUTPUT);
-  pinMode(MD2_BIN1, OUTPUT);
-  pinMode(MD2_BIN2, OUTPUT);
+  pinMode(FR_AIN1, OUTPUT);
+  pinMode(FR_AIN2, OUTPUT);
+  pinMode(FL_BIN1, OUTPUT);
+  pinMode(FL_BIN2, OUTPUT);
+  pinMode(BR_AIN1, OUTPUT);
+  pinMode(BR_AIN2, OUTPUT);
+  pinMode(BL_BIN1, OUTPUT);
+  pinMode(BL_BIN2, OUTPUT);
 
+  // pinMode(FR_PWM, OUTPUT);
+  // pinMode(FL_PWM, OUTPUT);
+  // pinMode(BR_PWM, OUTPUT);
+  // pinMode(BL_PWM, OUTPUT);
+  
   digitalWrite(MD1_STBY, HIGH);
   digitalWrite(MD2_STBY, HIGH);
   
-  ledcAttachPin(PWM1,      ledChannel1);
-  ledcAttachPin(PWM2,      ledChannel2);
-  ledcAttachPin(PWM3,      ledChannel3);
-  ledcAttachPin(PWM4,      ledChannel4);
+
 
 
 }
